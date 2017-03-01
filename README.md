02630bfa932bac0ada5d8ed8e91162191840b62c
Mart Lubbers <mart@martlubbers.net>
2017-03-01 14:44:23 +0100

    initial commit, rough draft, no authentication yet

---
+++ README.md
@@ -0,0 +1,33 @@
+# cloo.gl url shortener
+
+## How to setup:
+Copy all these files to some apache directory. Note that you have to enable
+`mod_rewrite` to have the full experience. Also the `sqlite3` library for `php`
+has to be available. The `Dockerfile` included can serve as a ready to use
+solution.
+
+## API calls:
+### GET
+- A GET without variables will forward the user to `cloogle.org`
+- A GET request with a key will forward the user to the url associated with the
+  key if available.
+
+### POST
+POST-variable `type` is required. When the request is successfull it will echo
+the shortened url.
+
+#### `type=regular`
+Regular url shortening service. Required arguments:
+- token
+
+	Authentication token, this service is not for everyone. If you want to use it
+	please contact one of the cloogle developers.
+- url
+
+	The url to be shortened
+
+#### `type=cloogle`
+Cloogle url shortening service. Required arguments:
+- url
+
+	The url to be shortened
--- index.php
+++ index.php
@@ -1,53 +1,83 @@
 <?php
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
 define("DBFILE", "db/db.sqlite");
-
-if($_SERVER['REQUEST_METHOD'] === 'GET'){
-	header('Location: https://cloogle.org');
-}
-
-if ($_SERVER['REQUEST_METHOD'] !== 'POST'){
-	echo "Unsupported request method...\n";
-	die();
-}
-
-if(!isset($_POST['type'])){
-	echo "No type set\n";
-	die();
-}
-
-switch($_POST['type']){
-case 'regular':
-	if(!isset($_POST['token'])){
-		echo "Authentication token required\n";
-		die();
-	}
-case 'cloogle':
-	if(!isset($_POST['url'])){
-		echo "url argument missing\n";
-		die();
-	}
-	break;
-default:
-	echo "Incorrect type\n";
-	die();
-}
 
 # Open handle
 if(!$db = new SQLite3(DBFILE)){
 	die();
 }
-
+	
 # Init the database
 $db->query("CREATE TABLE IF NOT EXISTS cloogle(url TEXT)");
 $db->query("CREATE TABLE IF NOT EXISTS regular(url TEXT)");
 $db->query("CREATE TABLE IF NOT EXISTS access(token TEXT)");
 
-if($_POST['type'] === 'cloogle'){
-	echo "Not implemented yet...\n";
+function quit($msg){
+	global $db;
+	echo $msg . "\n";
+	if(!is_null($db)){
+		$db->close();
+	}
+	exit;
 }
-if($_POST['type'] === 'regular'){
+
+# Lookup or just a redirect to cloogle.org
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+	$url = 'https://cloogle.org';
+	if(isset($_GET['key']) && isset($_GET['type'])){
+		if($_GET['type'] === 'regular'){
+			$stmt = $db->prepare("SELECT url FROM regular WHERE rowid=:id");
+			$stmt->bindValue(':id', intval(base64_decode($_GET['key'])), SQLITE3_INTEGER);
+			$res = $stmt->execute();
+			if($arr = $res->fetchArray()){
+				$url = $arr[0];
+				$stmt->close();
+			} else {
+				quit("No url with that key");
+			}
+		} else if($_GET['type'] === 'cloogle'){
+			quit("Not yet supported");
+		}
+	}
+	header("Location: $url");
+# Api call to generate a new link
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+	if(!isset($_POST['type'])){
+		quit("No type set");
+	}
+	
+	switch($_POST['type']){
+	case 'regular':
+		if(!isset($_POST['token'])){
+			quit("Authentication token required");
+		}
+	case 'cloogle':
+		if(!isset($_POST['url'])){
+			quit("url argument missing");
+		}
+		break;
+	default:
+		quit("Incorrect type");
+	}
+	
 	$stmt = $db->prepare("INSERT INTO regular (url) VALUES (:url)");
-	$stmt->bindParam(':url', $_POST['url'], SQLITE3_TEXT);
+	
+	if($_POST['type'] === 'cloogle'){
+		echo "Not implemented yet...\n";
+		$mod="e/";
+	}
+	
+	if($_POST['type'] === 'regular'){
+		$stmt->bindParam(':url', $_POST['url'], SQLITE3_TEXT);
+		$mod="";
+	}
+	
 	$stmt->execute();
-	echo "http://cloo.gl/" . base64_encode($db->lastInsertRowID()) . "\n";
+	echo "https://cloo.gl/" . $mod . base64_encode($db->lastInsertRowID()) . "\n";
+	$stmt->close();
+} else {
+	quit("Unsupported request method");
 }
+$db->close();
